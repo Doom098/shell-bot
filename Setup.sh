b8e89cdeb52de84f1e0e142b15874a49018735ae
Doom098 <74813697+Doom098@users.noreply.github.com>
2021-01-28 12:47:43 +0600

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,11 +1,16 @@
 echo "Performing additional setup 1"
-
-add-apt-repository -y ppa:poplite/qbittorrent-enhanced
-apt-get install qbittorrent-enhanced-nox -qq -y
+mkdir -p /root/.local/share/qBittorrent
+add-apt-repository ppa:qbittorrent-team/qbittorrent-stable
+apt-get install -y qbittorrent-nox
 curl https://rclone.org/install.sh | bash
 
 echo "Performing additional setup 2"
-wget -qq https://raw.githubusercontent.com/rafsanbasunia/qbittorrentWebUI/master/qbittorent2.conf \
-            -O /home/.config/qBittorrent/qBittorrent.conf
 
+cp -Ra /home/files/qBittorrent/ /root/.local/share/
 echo "Performing additional setup 3"
+
+apt-get update
+apt-get install -y tmate
+
+apt-get update
+apt-get install -y fuse
