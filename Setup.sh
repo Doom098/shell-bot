f5d6109ea3347b6124ad8d9c4bca4a10660b94d2
Doom098 <21doombringer@gmail.com>
2021-01-28 13:07:38 +0600

    did some change

---
+++ Setup.sh
@@ -0,0 +1,16 @@
+echo "Performing additional setup 1"
+mkdir -p /root/.local/share/qBittorrent
+add-apt-repository ppa:qbittorrent-team/qbittorrent-stable
+apt-get install -y qbittorrent-nox
+curl https://rclone.org/install.sh | bash
+
+echo "Performing additional setup 2"
+
+cp -Ra /home/files/qBittorrent/ /root/.local/share/
+echo "Performing additional setup 3"
+
+apt-get update
+apt-get install -y tmate
+
+apt-get update
+apt-get install -y fuse
