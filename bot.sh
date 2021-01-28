380b4562d821307ceff1d733139f248abec337a4
Doom098 <74813697+Doom098@users.noreply.github.com>
2021-01-28 13:18:37 +0600

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -2,15 +2,18 @@
 cd /home/shell-bot
 cat > config.json << EOF
 {
-    "authToken": "$BOT_TOKEN",
-    "owner": $OWN_ID
+    "authToken": "1527929353:AAERlylvfnoUCKttftF9mlWJqL4gRW7F8bw",
+    "owner": 1073166978
 }
 EOF
 echo "Created config for Shell bot"
+apt-get update
 
+rclone mount rex: /home/dl --allow-non-empty & ls
 
-# Sets your ngrok authtoken...
-ngrok authtoken "$NGROK_TOKEN"
+echo "Created mount for Shell bot"
+apt-get update
+yes y | qbittorrent-nox & ls
 
 echo "Starting Shell bot"
 node server
